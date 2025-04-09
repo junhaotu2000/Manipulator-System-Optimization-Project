3a8eac0b1cde0a83896c8da59cbed72302a50470
junhaotu <junhaotu@umich.edu>
2025-04-09 17:37:19 -0400

    change input to make model

--- test.m
+++
@@ -1,6 +0,0 @@
-clear; close all hidden;
-rotInertia = [0.02, 0.02, 0.01;
-              0.02, 0.02, 0.01;
-              0.02, 0.02, 0.01];
-mass = [1.0, 1.0, 1.0];
-[time_grid, I_eff, torque_traj, angle_traj] = PlannerFunction(rotInertia, mass);
